cec4b70974f38685b28264f973af49983c6a676d
Soumick Karmakar <70286775+Soumick-Karmakar@users.noreply.github.com>
2023-06-25 23:16:19 +0530

    Add files via upload

---
+++ HomePage.js
@@ -0,0 +1,11 @@
+document.getElementById("Home").onclick = function() {
+	location.href="http://127.0.0.1:5500/HomePage/HomePage.html"
+}
+
+document.getElementById("Login").onclick = function() {
+	location.href="http://127.0.0.1:5500/LoginPage/LoginPage.html"
+}
+
+document.getElementById("Register").onclick = function() {
+	location.href="http://127.0.0.1:5500/RegistrationPage/Register.html"
+}
